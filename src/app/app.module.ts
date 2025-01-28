5085ece44ab5882de9edc6fb0c63f2c2be825ea5
Carl <rauzertime123@gmail.com>
2025-01-28 17:53:18 +0800

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post.create.component';
+import { MatCardModule } from '@angular/material/card'; // Correct import for MatCardModule
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
