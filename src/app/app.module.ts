f7c10032167fe6226ad5016d2b3cee8485ed2661
Carl <rauzertime123@gmail.com>
2025-02-25 16:40:54 +0800

    backend%server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post.create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post.list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatCardModule } from '@angular/material/card'; 
 import { MatExpansionModule} from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
+import { PostsService } from './posts/posts.service';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,8 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatToolbarModule,
     MatInputModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
