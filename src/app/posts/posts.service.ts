c702e04343963cf749b1bf2c9f1b4d9e99f16ba7
Carl <rauzertime123@gmail.com>
2025-02-28 16:27:02 +0800

    Exam

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -13,7 +13,8 @@ export class PostsService {
     }
 
     getPosts() {  
-        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+        this.http.get<{message: string, posts: Post[]}>
+        ('http://localhost:3000/api/posts')
         .subscribe((postData) => {
             this.posts = postData.posts;
             this.postsUpdated.next([...this.posts]);
@@ -24,9 +25,14 @@ export class PostsService {
         return this.postsUpdated.asObservable();
     }
 
-    addPost(title: string, content: string) {
+    addPost(title: string, content:string){
         const post: Post = {id: null, title: title, content: content};
-        this.posts.push(post);
-        this.postsUpdated.next([...this.posts]);
+        this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+        .subscribe((responseData) => {
+            console.log(responseData.message);
+            this.posts.push(post);
+            this.postsUpdated.next([...this.posts]);
+        });
+
     }
-}
+    }
