8d5bfe3870e2adf43ef6b14d55bf4d024b605b76
Carl <rauzertime123@gmail.com>
2025-03-04 16:55:04 +0800

    DB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const app = express();
 const bosyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
+const app = express();
+
+
+mongoose.connect("mongodb+srv://rauzertime123:Rauzer17@project.dbdij.mongodb.net/?retryWrites=true&w=majority&appName=Project")
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch(() => {
+        console.log('Connection failed!');
+    })
+    
 app.use(bosyParser.json());
 app.use(bosyParser.urlencoded({extended: false}));
 
@@ -27,7 +39,7 @@ app.use('/api/posts', (req, res, next) => {
     const posts = 
         [{
             id: "fadf12421l",
-            title: "First server-side post",
+            title: "First server-s",
             content: "first from server-side"
         },
         {
